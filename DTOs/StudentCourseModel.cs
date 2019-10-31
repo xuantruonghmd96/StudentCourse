21aa599c65576c69a59c61262f509930b9252fe0
nguyendaoxuantruong <nguyendaoxuantruong@secure.janeto.com>
2019-10-31 23:26:04 +0700

    hot fix

--- DTOs/StudentCourseModel.cs
+++ DTOs/StudentCourseModel.cs
@@ -8,15 +8,18 @@ namespace StudentCourse.DTOs
 {
     public class StudentCourseModel
     {
-        public int CourseId { get; set; }
-        public string CourseName { get; set; }
+        //public int CourseId { get; set; }
+        //public string CourseName { get; set; }
         public int Donated { get; set; }
+
+        public CourseModel Course { get; set; }
 
         public StudentCourseModel(StudentCourseMap studentCourseMap)
         {
-            this.CourseId = studentCourseMap.Course == null ? 0 : studentCourseMap.Course.Id;
-            this.CourseName = studentCourseMap.Course?.Name;
+            //this.CourseId = studentCourseMap.Course == null ? 0 : studentCourseMap.Course.Id;
+            //this.CourseName = studentCourseMap.Course?.Name;
             this.Donated = studentCourseMap.Donated;
+            this.Course = new CourseModel(studentCourseMap.Course);
         }
     }
 }
